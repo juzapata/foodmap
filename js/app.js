6e6c100a12e01b3f60fd620589e9ad3d7bc2c88e
Julia Zapata <juliamzapata@gmail.com>
2018-08-06 09:27:23 -0300

    ultimos mudanças no estilo

--- js/app.js
+++ js/app.js
@@ -14,63 +14,65 @@ $(document).ready(function() {
     map = new google.maps.Map(document.getElementById("map"), options);
   }
   initialize();
+  
   function putPin(){
     $.each(restaurantes, function(index, restaurantsLoop) { 
-      var marker = new google.maps.Marker({
-	  position: new google.maps.LatLng(restaurantsLoop.latitude, restaurantsLoop.longitude),
-	  title: "Eai, qual você vai escolher? Esse é bom hein!",
-	  map: map
-	  });
+      // função para colocar marcador no mapa
+	  var marker = new google.maps.Marker({
+	    position: new google.maps.LatLng(restaurantsLoop.latitude, restaurantsLoop.longitude),
+	    title: "Eai, qual você vai escolher? Esse é bom hein!",
+	    map: map
+	});
+	// pegando informaçõs no data e html
     var images = restaurantsLoop.image;
-	
 	var type = restaurantsLoop.type;
 	var inputValue = $('.food-input').val();
-	
-	 $(".restaurantes").append($("<img>").attr("src", restaurantsLoop.image).attr('id', restaurantsLoop.name).addClass(restaurantsLoop.type).append(restaurantsLoop.type));
-     
- 
- $(".filter").click(function() {
-  var inputValue = $('.food-input').val();
-  
-  $("img").each(function () {
-  if($(this).text() !== inputValue){
-    $(this).fadeOut('slow');	  
-  }	
-});
-
-  });
-    $('.food-input').on('input', function() {
+	var descriptions = restaurantsLoop.description;	
+	var names = restaurantsLoop.name;
+	// colocando as imagens
+	$(".restaurantes").append($("<img>").attr("src", restaurantsLoop.image).attr('id', restaurantsLoop.name).addClass(restaurantsLoop.type).append(restaurantsLoop.type));
+	// função para desaparecer restaurantes não selecionados
+    $(".filter").click(function() {
+      var inputValue = $('.food-input').val();
+      $("img").each(function () {
+        if($(this).text() !== inputValue){
+          $(this).fadeOut('slow');	  
+        }	
+      });
+    });
+    // função para reaparecer as imagens
+	$('.food-input').on('input', function() {
 	  if($(this).val() === ""){
 		$('img').each(function(){
 		  $(this).fadeIn('slow');	
 		});  
 	  }
-  }); 
-  
-  });
- 
- }
-
+    }); 
+    // função para aparece modal
+	$("img").click(function(){
+		  $(this).each(function (){
+			  // alert(restaurantsLoop.description);
+			  $(".window-popup").show(300);
+		  });	
+			  });
+      
+    });
+  } 
+  // chamand a função
   putPin();
-
+  // coloquei a classe para setar i layout das imagens
   $("img").each(function (){
     $(this).addClass("img-set");	  
   });  
 
+  // function showModal (){
+    // $.each(restaurantes, function(index, restaurantsLoop) {
+	  
+	  // 
+	
+	// });  
+  // }  
+  
 });
   
-   // $('.filter').click(function() {
-    // type.each(function () {
-    // if(type.value !== inputValue){
-      // type.fadeOut('slow');	  
-    // }	
-  // });
-// }); 
-  
- // $('.choice').on('input', function() {
-    // if($(this).val() === ""){
-	  // $('li').each(function(){
-	    // $(this).fadeIn('slow');	
-		// });  
-	  // }
-  // });  
+ 
